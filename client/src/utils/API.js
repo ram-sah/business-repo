93cc15fe42bb2585a1355666fadc497ba2b7d024
Ram Sah <mohan2036@gmail.com>
2020-10-15 21:23:07 -0400

    backend

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,4 +4,23 @@ export default {
   getBusinesses: function() {
     return axios.get("/api/businesses");
   }
+
+}
+
+function getPosition() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+    //console.log(showPosition);
+  }
+  function showPosition(position) {
+    console.log(
+      "Latitude: " +
+        position.coords.latitude +
+        " " +
+        "Longitude: " +
+        position.coords.longitude
+    );
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+  }
 }
